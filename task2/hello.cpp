837a0df1618d07c522972b831d807c7463bd9ff9
2334918435 <111514212+2334918435@users.noreply.github.com>
2025-10-23 21:53:40 +0800

    Add files via upload

---
+++ task2/hello.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+    cout << "Hello World!"<< endl;
+    return 0;
+}
+// #include <iostream>
+// using namespace std;
+//
+// int main()
+// {
+//     cout << __cplusplus << endl; // 假设我的 C++ 版本是 199711，对比下面的参数是 C++98
+// }
